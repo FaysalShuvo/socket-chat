c81c4c56635500ea2a5d3e5ac79b8c239138f3a8
shuvonma57@gmail.com <FaysalShuvo>
2021-06-19 22:39:32 +0600

    finished

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,14 +9,10 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message", +msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnect");
+    io.emit("chat message", msg);
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
